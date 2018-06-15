14d1b31e9a2d349be6db5f0d44dc4df944236eb4
Stéfano Girardelli <girardellisb@gmail.com>
2018-06-15 17:38:55 -0300

    Adding Emoji flags to each country on the console log

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const fetch = require('node-fetch')
+const fetch = require('node-fetch');
+const translate = require('moji-translate');
 
 function status (response) {
   if (response.status >= 200 && response.status < 300) {
@@ -18,7 +19,7 @@ fetch('http://worldcup.sfg.io/matches')
   .then(matches => {
     matches.map(match => {
       if (match.status == 'completed' || match.status == 'in progress')
-        console.log(`${match.home_team.country} ${match.home_team.goals} x ${match.away_team.goals} ${match.away_team.country}`)
+        console.log(translate.translate(`${match.home_team.country}`.replace(/ /g,'_'), true) + ` ${match.home_team.country} ${match.home_team.goals} x ${match.away_team.goals} ${match.away_team.country} ` + translate.translate(`${match.away_team.country}`.replace(/ /g,'_'), true));
     })
   })
   .catch(error => console.log('Request failed', error))
